40ea074aa9796ee1a362cf63f914f82bbecb7254
Louis <louisdemeslayau@gmail.com>
2019-01-17 22:24:03 +0800

    Init commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require ('express');
+const hbs = require('hbs');
+const fs = require ('fs');
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+})
+
+
+app.set('view engine', 'hbs');
+app.use((req,res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('log' , log + ' \\n ',  function(error){
+      console.log('error')
+    }
+  )
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+  res.render('welcome.hbs', {
+    pageTitle:'about page',
+    welcomeMessage : 'pute pute pute pute',
+
+
+})});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs', {
+    pageTitle:'about page',
+  });
+});
+
+app.get('/bad', (req,res)=>{
+  res.send({
+    errorMessage : 'unable to find data'
+  })
+})
+
+app.listen(3000, console.log('server ready'))
